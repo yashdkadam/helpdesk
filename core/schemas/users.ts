b0241993b0309bbd5327aa33f2518dc4c22567c4
Yash Kadam <kadamdyash55@gmail.com>
2026-05-02 12:06:00 +0530

    use react query

--- core/schemas/users.ts
+++ core/schemas/users.ts
@@ -2,7 +2,7 @@ import { z } from "zod/v4";
 
 export const createUserSchema = z.object({
   name: z.string().min(1),
-  email: z.string().email(),
+  email: z.string().email(), 
   password: z.string().min(8),
 });
 
